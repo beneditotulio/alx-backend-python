c805722e462032e82fe11d0cc6ab0f370804a563
Túlio Benedito Nhantumbo <80028633+beneditotulio@users.noreply.github.com>
2023-10-06 16:50:05 +0200

    1. Basic annotations - concat

---
+++ 0x00-python_variable_annotations/1-concat.py
@@ -0,0 +1,11 @@
+#!/usr/bin/env python3
+"""
+1-concat module
+Basic annotation example
+"""
+
+
+def concat(str1: str, str2: str) -> str:
+    """Simple function to concat 2 strings
+    """
+    return str1 + str2
