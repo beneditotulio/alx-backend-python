b2ae333db8f23007b76b1afecb6fdf2d3bebcae2
Túlio Benedito Nhantumbo <80028633+beneditotulio@users.noreply.github.com>
2023-10-06 16:49:53 +0200

    0. Basic annotations - add

---
+++ 0x00-python_variable_annotations/0-add.py
@@ -0,0 +1,12 @@
+#!/usr/bin/env python3
+"""
+0-add module
+Basic annotation example
+"""
+
+
+def add(a: float, b: float) -> float:
+    """simple function that adds 2 numbers
+    Basic annotation
+    """
+    return a + b
